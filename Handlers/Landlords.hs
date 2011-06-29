3120eadf11d2b9ca2e90e509cb91f40e17c0a2ae
patrick brisbin <pbrisbin@gmail.com>
2011-06-29 19:45:29 -0400

    flesh out landlords page

--- Handlers/Landlords.hs
+++ Handlers/Landlords.hs
@@ -7,17 +7,74 @@ import Model
 import Yesod
 import Database.Persist.Base
 import Yesod.Comments
-import Yesod.Goodies.Markdown
-import Yesod.Goodies.Time
+import Yesod.Goodies
 import qualified Data.Text as T
 import qualified Settings
 
 getLandlordsR :: Key Landlord -> Handler RepHtml
 getLandlordsR lid = do
     docs <- siteDocs =<< getYesod
-    defaultLayout $ do
-        Settings.setTitle "View landlord"
-        [hamlet| Landlord page |]
+    let ldocs = filterDocs lid docs
+
+    -- show the no reviews page, 404 if ll doesn't exist at all
+    if null ldocs
+        then noReviews =<< runDB (get404 lid)
+        else do
+            -- known to be safe
+            let l = landlord $ head ldocs
+            let g = gpa $ map (reviewGrade . review) ldocs
+            
+            defaultLayout $ do
+                Settings.setTitle . T.unpack $ landlordName l
+                [hamlet|
+                    <div .tabdiv>
+                        <div .view-landlord>
+                            <div .title>
+                                <p>
+                                    #{landlordName l}
+                                    <span .grade>GPA: #{show $ g}
+
+                            <div .reviews>
+                            $forall d <- ldocs
+                                ^{listDocument d}
+                    |]
+
+noReviews :: Landlord -> Handler RepHtml
+noReviews l = defaultLayout $ do
+    Settings.setTitle . T.unpack $ landlordName l
+    [hamlet|
+        <div .tabdiv>
+            <div .view-landlord>
+                <p>
+                    I'm sorry, #{landlordName l} has not been reviewed 
+                    yet.
+
+                <p>
+                    Would you like to 
+                    <a href="@NewR@landlord?#{landlordName l}">write one
+                    ?
+        |]
+
+listDocument :: Document -> Widget ()
+listDocument (Document rid r l u) = do
+    reviewTime <- lift . humanReadableTime $ reviewCreatedDate r
+    let content = markdownToHtml . shorten 400 $ reviewContent r
+    [hamlet|
+        <div .review>
+            <div .title>
+                Reviewed by #{showName u} #{reviewTime}
+                <span .grade>
+                    #{prettyGrade $ reviewGrade r}
+
+            <div .address>
+                #{reviewAddress r}
+
+            <div .review>
+                #{content}
+        |]
+
+filterDocs :: LandlordId -> [Document] -> [Document]
+filterDocs lid = filter ((lEq lid) . reviewLandlord . review)
 
 -- | Somehow related to the persistent upgrade, keys are stored as 
 --   PersistInt64 Int64 but when used as a singlePiece they come in as 
