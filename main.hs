2147cc8be02512cb9b58a246f42c7b54d49dda96
patrick brisbin <pbrisbin@gmail.com>
2012-01-19 19:30:57 -0500

    fix main for new yesod, grrr

--- main.hs
+++ main.hs
@@ -1,7 +1,11 @@
-import Prelude              (IO)
+import Prelude
 import Yesod.Default.Config (fromArgs)
 import Yesod.Default.Main   (defaultMain)
-import Application          (withRenters)
+import Application          (getApplication)
 
 main :: IO ()
-main = defaultMain fromArgs withRenters
+main = defaultMain (fromArgs parseNothing) getApplication
+
+-- I don't need the stupid Extra
+parseNothing :: Monad m => a -> b -> m ()
+parseNothing _ _ = return ()
