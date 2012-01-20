f336875878deaf7822d10a9860da617d46b1d24c
patrick brisbin <pbrisbin@gmail.com>
2012-01-19 19:30:12 -0500

    add joinTables helper to solve N+1

--- Helpers/Model.hs
+++ Helpers/Model.hs
@@ -1,7 +1,12 @@
-module Helpers.Model (findOrCreate) where
+module Helpers.Model
+    ( findOrCreate
+    , joinTables
+    ) where
 
 import Prelude
 import Yesod
+import Data.Maybe (catMaybes)
+import qualified Data.Map as M
 
 findOrCreate :: (YesodPersist m,
                  PersistUnique (YesodPersistBackend m) (GHandler s m),
@@ -9,3 +14,34 @@ findOrCreate :: (YesodPersist m,
                  PersistEntity v)
              => v -> GHandler s m (Key (YesodPersistBackend m) v)
 findOrCreate v = return . either entityKey id =<< runDB (insertBy v)
+
+-- |
+--
+-- My solution to the N+1 problem:
+--
+-- > runDB $ do
+-- >     posts <- selectList [] []
+-- >     users <- selectList [] []
+-- >
+-- >     let records = joinTables postUser posts users
+-- >
+-- >     forM records $ \(post,user) -> do
+-- >         --
+-- >         -- ...
+-- >         --
+--
+joinTables :: (a -> Key backend b)
+           -> [Entity backend a]
+           -> [Entity backend b]
+           -> [(Entity backend a, Entity backend b)]
+joinTables f as bs = catMaybes $ map (joinRelation f bs) as
+
+    where
+        joinRelation :: (a -> Key backend b) -> [Entity backend b] -> Entity backend a -> Maybe (Entity backend a, Entity backend b)
+        joinRelation f bs a = fmap (\b -> (a,b)) $ lookupRelation (f $ entityVal a) (toMap bs)
+
+        lookupRelation :: Key backend b -> M.Map (Key backend b) b -> Maybe (Entity backend b)
+        lookupRelation k = fmap (\v -> Entity k v) . M.lookup k
+
+        toMap :: [Entity backend a] -> M.Map (Key backend a) a
+        toMap = M.fromList . map (\(Entity k v) -> (k,v))
