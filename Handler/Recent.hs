767357b0d8b45dc976db3b52532cea1ac9a7997b
patrick brisbin <pbrisbin@gmail.com>
2012-02-05 13:43:57 -0500

    flesh out recent feed

--- Handler/Recent.hs
+++ Handler/Recent.hs
@@ -1,6 +1,30 @@
 module Handler.Recent (getRecentR) where
 
 import Import
+import Yesod.Markdown
+import Helpers.Model
+import Control.Monad (forM)
+import qualified Data.Text as T
 
 getRecentR :: Handler RepJson
-getRecentR = undefined
+getRecentR = do
+    records <- runDB $ do
+        reviews <- selectList [] [Desc ReviewCreatedDate, LimitTo 5]
+
+        let lids = map (reviewLandlord . entityVal) reviews
+
+        landlords <- selectList [LandlordId <-. lids] []
+
+        return $ joinTables reviewLandlord reviews landlords
+
+    objects <- forM records $ \(review,landlord) -> do
+        return $ object [ ("landlord", landlordName $ entityVal landlord)
+                        , ("content",  shorten 200 . reviewContent $ entityVal review)
+                        , ("link",     "/reviews/" `T.append` (toPathPiece $ entityKey review))
+                        ]
+
+    jsonToRepJson $ array objects
+
+    where
+        shorten :: Int -> Markdown -> Text
+        shorten n (Markdown s) = T.pack $ if length s >= n then take n s ++ "..." else s
