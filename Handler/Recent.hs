af2200bf151863e15e742754e0a18f1a578359cd
patrick brisbin <pbrisbin@gmail.com>
2012-02-06 09:37:00 -0500

    flesh out recent slideshow

--- Handler/Recent.hs
+++ Handler/Recent.hs
@@ -3,24 +3,28 @@ module Handler.Recent (getRecentR) where
 import Import
 import Yesod.Markdown
 import Helpers.Model
+import Helpers.User
 import Control.Monad (forM)
 import qualified Data.Text as T
 
 getRecentR :: Handler RepJson
 getRecentR = do
     records <- runDB $ do
-        reviews <- selectList [] [Desc ReviewCreatedDate, LimitTo 5]
+        reviews <- selectList [] [Desc ReviewCreatedDate, LimitTo 10]
 
         let lids = map (reviewLandlord . entityVal) reviews
+        let uids = map (reviewReviewer . entityVal) reviews
 
         landlords <- selectList [LandlordId <-. lids] []
+        users     <- selectList [UserId <-. uids] []
 
-        return $ joinTables reviewLandlord reviews landlords
+        return $ joinTables3 reviewLandlord reviewReviewer reviews landlords users
 
-    objects <- forM records $ \(review,landlord) -> do
-        return $ object [ ("landlord", landlordName $ entityVal landlord)
-                        , ("content",  shorten 200 . reviewContent $ entityVal review)
-                        , ("link",     "/reviews/" `T.append` (toPathPiece $ entityKey review))
+    objects <- forM records $ \(review,landlord,user) -> do
+        return $ object [ ("landlord",                        landlordName $ entityVal landlord)
+                        , ("content" ,         shorten 200 . reviewContent $ entityVal review  )
+                        , ("user"    ,                            showName $ entityVal user    )
+                        , ("link"    , "/reviews/" `T.append` (toPathPiece $ entityKey review) )
                         ]
 
     jsonToRepJson $ array objects
