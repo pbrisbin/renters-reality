199b2761777bdfbc3e7bd956a212f26750a95d6e
patrick brisbin <pbrisbin@gmail.com>
2012-06-13 18:51:42 -0400

    export Aeson, hide Yesod.object

--- Import.hs
+++ Import.hs
@@ -1,3 +1,6 @@
+-- NOTE: I choose to export and use Data.Aeson.Types.object which has
+-- different semantics than Yesod.JSON.object but allows for mixed types
+-- to be the object values.
 module Import
     ( module Prelude
     , module Yesod
@@ -5,15 +8,17 @@ module Import
     , (<>)
     , Text
     , module Data.Monoid
+    , module Data.Aeson.Types
     , module Control.Applicative
     ) where
 
 import Prelude hiding (writeFile, readFile)
-import Yesod hiding (Route(..), setTitle)
+import Yesod hiding (Route(..), setTitle, object)
 import Foundation
 import Data.Monoid (Monoid (mappend, mempty, mconcat))
 import Control.Applicative ((<$>), (<*>), pure)
 import Data.Text (Text)
+import Data.Aeson.Types
 
 infixr 5 <>
 (<>) :: Monoid m => m -> m -> m
