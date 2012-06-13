17af35eb59401fc44fc9fdefe88076e8aea2cca0
patrick brisbin <pbrisbin@gmail.com>
2012-06-13 18:51:21 -0400

    add ToJSON instances for models

--- Model.hs
+++ Model.hs
@@ -1,11 +1,13 @@
 module Model where
 
 import Prelude
-import Yesod
+import Yesod hiding (object)
 import Yesod.Markdown
 import Data.Text (Text)
-
+import qualified Data.Text as T
+import Data.Aeson.Types
 import Data.Time (UTCTime(..))
+import Text.Shakespeare.Text (st)
 
 data Grade = Aplus | A | Aminus
            | Bplus | B | Bminus
@@ -21,3 +23,33 @@ derivePersistField "Grade"
 -- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
     $(persistFile "config/models")
+
+instance ToJSON Grade where
+    toJSON Aplus   = String "A+"
+    toJSON Aminus  = String "A-"
+    toJSON Bplus   = String "B+"
+    toJSON Bminus  = String "B-"
+    toJSON Cplus   = String "C+"
+    toJSON Cminus  = String "C-"
+    toJSON Dplus   = String "D+"
+    toJSON Dminus  = String "D-"
+    toJSON g       = String . T.pack $ show g
+
+instance ToJSON (Entity User) where
+    toJSON (Entity uid u) = object [ "link"     .= [st|/users/#{toPathPiece uid}/|]
+                                   , "username" .= (case (userFullname u, userUsername u) of
+                                                        (Just fn, _      ) -> fn
+                                                        (_,       Just un) -> un
+                                                        _                  -> "anoymous")
+                                   ]
+
+instance ToJSON (Entity Landlord) where
+    toJSON (Entity lid l) = object [ "link" .= [st|/landlords/#{toPathPiece lid}/|]
+                                   , "name" .= (landlordName l)
+                                   ]
+
+instance ToJSON (Entity Review) where
+    toJSON (Entity rid r) = object [ "link"   .= [st|/reviews/#{toPathPiece rid}/|]
+                                   , "grade"  .= (reviewGrade r)
+                                   , "content".= ((\(Markdown s) -> T.pack s) $ reviewContent r)
+                                   ]
