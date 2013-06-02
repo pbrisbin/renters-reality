d53f1357aa87afe455678f38645199114b48eeac
patrick brisbin <pbrisbin@gmail.com>
2013-06-02 19:36:48 -0400

    Remove accidentially committed tempfiles

--- static/tmp/autogen-UvQeO7_a.js
+++
@@ -1,1 +0,0 @@
-$(function(){$('#search-input').autocomplete({source:"http://localhost:3000/search/comp/searches",minLength:3});$('#landlord-input').autocomplete({source:"http://localhost:3000/search/comp/landlords",minLength:3})})
